0ca5da6845a92111238ca0726dd7d167f29f083c
adviorina <149464114+adviorina@users.noreply.github.com>
2025-04-13 10:43:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let thirdVarList = document.querySelector("ul").lastElementChild;
+thirdVarList.innerHTML = "Mixie"; 
+
+let firstAnchor = document.querySelector("li a").style.color = "red";
+
+document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add('huge');
